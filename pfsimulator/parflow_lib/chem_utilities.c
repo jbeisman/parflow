409d591c18cdd8b4f1ec5e10d056bf1d2c157576
jbeisman <jbeisman@lanl.gov>
2019-06-15 02:57:20 -0600

    utilities to read and write single pfb for chem restarts

--- pfsimulator/parflow_lib/chem_utilities.c
+++ pfsimulator/parflow_lib/chem_utilities.c
@@ -222,28 +222,370 @@ void SelectReactTransTimeStep(double max_velocity, double CFL,
 
 void CutTimeStepandSolveSingleCell(AlquimiaInterface chem, AlquimiaState *chem_state, AlquimiaProperties *chem_properties, void *chem_engine, AlquimiaAuxiliaryData *chem_aux_data, AlquimiaEngineStatus *chem_status, double original_dt)
 {
-  double tenth_dt;  
-  tenth_dt = 0.1 * original_dt;
-
-  amps_Printf("Original dt of %e seconds reduced to %e seconds.\n",original_dt,tenth_dt);
+  double tenth_dt = 0.1 * original_dt;
+  amps_Printf("Original dt of %e seconds failed. Attempting to solve 10 smaller timesteps of %e seconds.\n",original_dt,tenth_dt);
 
   for (int i = 0; i < 10; i++)
   {
       // Solve the geochemical system with a dt of half the original_dt
       chem.ReactionStepOperatorSplit(&chem_engine,
-                                           tenth_dt, chem_properties,
-                                           chem_state,
-                                           chem_aux_data,
-                                           chem_status);
+                                    tenth_dt, chem_properties,
+                                    chem_state,
+                                    chem_aux_data,
+                                    chem_status);
    if (!chem_status->converged)
     {
-
       amps_Printf("Geochemical engine failed to converge. Timestep halved 10 times to %e seconds.\n",tenth_dt);
       PARFLOW_ERROR("Geochemical engine error, exiting simulation.\n");
-
     }
   }
 }
 
 
+void WriteChemChkpt(Grid *grid, 
+                   ProblemData *problem_data,
+                   AlquimiaSizes *chem_sizes,
+                   AlquimiaState *chem_state,
+                   AlquimiaAuxiliaryData *chem_aux_data,
+                   AlquimiaProperties *chem_properties,
+                   char *file_prefix,
+                   char *file_suffix)
+{
+  SubgridArray   *subgrids = GridSubgrids(grid);
+  Subgrid        *subgrid;
+  GrGeomSolid    *gr_domain;
+
+  int num_chem_doubles, num_chem_ints;
+  int is;
+  int p;
+  int num_cells;
+  long size;
+
+  char file_extn[7] = "pfb";
+  char filename[255];
+  amps_File file;
+
+  BeginTiming(PFBTimingIndex);
+
+  gr_domain = ProblemDataGrDomain(problem_data);
+  p = amps_Rank(amps_CommWorld);
+
+  if (p == 0)
+    size = 6 * amps_SizeofDouble + 4 * amps_SizeofInt;
+  else
+    size = 0;
+
+  // how big is the chemistry problem?
+  num_chem_ints = chem_sizes->num_aux_integers;
+  num_chem_doubles = chem_sizes->num_primary + // total mobile
+                     chem_sizes->num_minerals * 3 + // volfx, ssa, rate constant
+                     chem_sizes->num_surface_sites + // surface sites
+                     chem_sizes->num_ion_exchange_sites + // ion exchange sites 
+                     chem_sizes->num_isotherm_species * 3 + // isotherm, Freundlich, Langmuir
+                     chem_sizes->num_aqueous_kinetics + // aqueous rate constant
+                     chem_sizes->num_aux_doubles; // aux data doubles
+  if (chem_sizes->num_sorbed > 0)
+  {
+    num_chem_doubles += chem_sizes->num_primary; // sorbed immobile primary
+  }
+
+  ForSubgridI(is, subgrids)
+  {
+      size = 9 * amps_SizeofInt;
+  }
+
+  num_cells = SubgridNumCells(grid, problem_data);
+  size += num_cells * num_chem_doubles * amps_SizeofDouble;
+  size += num_cells * num_chem_ints * amps_SizeofInt;
+
+  /* open file */
+  sprintf(filename, "%s.%s.%s", file_prefix, file_suffix, file_extn);
+
+  if ((file = amps_FFopen(amps_CommWorld, filename, "wb", size)) == NULL)
+  {
+    amps_Printf("Error: can't open output file %s\n", filename);
+    exit(1);
+  }
+
+  /* Compute number of patches to write */
+  int num_subgrids = GridNumSubgrids(grid);
+  {
+    amps_Invoice invoice = amps_NewInvoice("%i", &num_subgrids);
+
+    amps_AllReduce(amps_CommWorld, invoice, amps_Add);
+
+    amps_FreeInvoice(invoice);
+  }
+
+
+  if (p == 0)
+  {
+    amps_WriteDouble(file, &BackgroundX(GlobalsBackground), 1);
+    amps_WriteDouble(file, &BackgroundY(GlobalsBackground), 1);
+    amps_WriteDouble(file, &BackgroundZ(GlobalsBackground), 1);
+
+#if 0
+    amps_WriteInt(file, &BackgroundNX(GlobalsBackground), 1);
+    amps_WriteInt(file, &BackgroundNY(GlobalsBackground), 1);
+    amps_WriteInt(file, &BackgroundNZ(GlobalsBackground), 1);
+#else
+    amps_WriteInt(file, &SubgridNX(GridBackground(grid)), 1);
+    amps_WriteInt(file, &SubgridNY(GridBackground(grid)), 1);
+    amps_WriteInt(file, &SubgridNZ(GridBackground(grid)), 1);
+#endif
+
+    amps_WriteDouble(file, &BackgroundDX(GlobalsBackground), 1);
+    amps_WriteDouble(file, &BackgroundDY(GlobalsBackground), 1);
+    amps_WriteDouble(file, &BackgroundDZ(GlobalsBackground), 1);
+
+    amps_WriteInt(file, &num_subgrids, 1);
+  }
+
+
+  ForSubgridI(is, subgrids)
+  {
+    subgrid = SubgridArraySubgrid(subgrids, is);
+
+    int i, j, k;
+    int ix = SubgridIX(subgrid);
+    int iy = SubgridIY(subgrid);
+    int iz = SubgridIZ(subgrid);
+
+    int nx = SubgridNX(subgrid);
+    int ny = SubgridNY(subgrid);
+    int nz = SubgridNZ(subgrid);
+
+    int r = SubgridRX(subgrid);
+
+
+    amps_WriteInt(file, &ix, 1);
+    amps_WriteInt(file, &iy, 1);
+    amps_WriteInt(file, &iz, 1);
+
+    amps_WriteInt(file, &nx, 1);
+    amps_WriteInt(file, &ny, 1);
+    amps_WriteInt(file, &nz, 1);
+
+    amps_WriteInt(file, &SubgridRX(subgrid), 1);
+    amps_WriteInt(file, &SubgridRY(subgrid), 1);
+    amps_WriteInt(file, &SubgridRZ(subgrid), 1);
+
+    GrGeomInLoop(i, j, k, gr_domain, r, ix, iy, iz, nx, ny, nz,
+    {
+      int chem_index = (i-ix) + (j-iy) * nx + (k-iz) * nx * ny;
+
+      for (int idx = 0; idx < chem_sizes->num_primary; idx++)
+      {
+        amps_WriteDouble(file, &chem_state[chem_index].total_mobile.data[idx], 1);
+      }
+
+
+      for (int idx = 0; idx < chem_sizes->num_minerals; idx++)
+      {
+        amps_WriteDouble(file, &chem_state[chem_index].mineral_volume_fraction.data[idx], 1);
+        amps_WriteDouble(file, &chem_state[chem_index].mineral_specific_surface_area.data[idx], 1);
+        amps_WriteDouble(file, &chem_properties[chem_index].mineral_rate_cnst.data[idx], 1);
+      }
+
+
+      for (int idx = 0; idx < chem_sizes->num_surface_sites; idx++)
+      {
+        amps_WriteDouble(file, &chem_state[chem_index].surface_site_density.data[idx], 1);
+      }
+
+
+      for (int idx = 0; idx < chem_sizes->num_ion_exchange_sites; idx++)
+      {
+        amps_WriteDouble(file, &chem_state[chem_index].cation_exchange_capacity.data[idx], 1);
+      }
+
+
+      for (int idx = 0; idx < chem_sizes->num_isotherm_species; idx++)
+      {
+        amps_WriteDouble(file, &chem_properties[chem_index].isotherm_kd.data[idx], 1);
+        amps_WriteDouble(file, &chem_properties[chem_index].freundlich_n.data[idx], 1);
+        amps_WriteDouble(file, &chem_properties[chem_index].langmuir_b.data[idx], 1);
+      }
+
+
+      for (int idx = 0; idx < chem_sizes->num_aqueous_kinetics; idx++)
+      {
+        amps_WriteDouble(file, &chem_properties[chem_index].aqueous_kinetic_rate_cnst.data[idx], 1);
+      }
+
+
+      for (int idx = 0; idx < chem_sizes->num_aux_doubles; idx++)
+      {
+        amps_WriteDouble(file, &chem_aux_data[chem_index].aux_doubles.data[idx], 1);
+      }
+
+
+      if (chem_sizes->num_sorbed > 0)
+      {
+        for (int idx = 0; idx < chem_sizes->num_primary; idx++)
+          amps_WriteDouble(file, &chem_state[chem_index].total_immobile.data[idx], 1);
+      }
+
+
+      for (int idx = 0; idx < chem_sizes->num_aux_integers; idx++)
+      {
+        amps_WriteInt(file, &chem_aux_data[chem_index].aux_ints.data[idx], 1);
+      }
+
+    });
+
+  }
+  amps_FFclose(file);
+  EndTiming(PFBTimingIndex);
+}
+
+
+
+void ReadChemChkpt(Grid *grid, 
+                   ProblemData *problem_data,
+                   AlquimiaSizes *chem_sizes,
+                   AlquimiaState *chem_state,
+                   AlquimiaAuxiliaryData *chem_aux_data,
+                   AlquimiaProperties *chem_properties,
+                   char *filename)
+{
+  SubgridArray   *subgrids = GridSubgrids(grid);
+  Subgrid        *subgrid;
+  GrGeomSolid    *gr_domain;
+
+  int is;
+  int p, P;
+  int num_chars;
+  
+  amps_File file;
+  double X, Y, Z;
+  int NX, NY, NZ;
+  double DX, DY, DZ;
+
+  BeginTiming(PFBTimingIndex);
+  
+  gr_domain = ProblemDataGrDomain(problem_data);
+  p = amps_Rank(amps_CommWorld);
+  P = amps_Size(amps_CommWorld);
+
+  if (((num_chars = strlen(filename)) < 4) ||
+      (strcmp(".pfb", &filename[num_chars - 4])))
+  {
+    amps_Printf("Error: %s is not in pfb format\n", filename);
+    exit(1);
+  }
+
+  if ((file = amps_FFopen(amps_CommWorld, filename, "rb", 0)) == NULL)
+  {
+    amps_Printf("Error: can't open input file %s\n", filename);
+    exit(1);
+  }
+
+  if (p == 0)
+  {
+    amps_ReadDouble(file, &X, 1);
+    amps_ReadDouble(file, &Y, 1);
+    amps_ReadDouble(file, &Z, 1);
+
+    amps_ReadInt(file, &NX, 1);
+    amps_ReadInt(file, &NY, 1);
+    amps_ReadInt(file, &NZ, 1);
+
+    amps_ReadDouble(file, &DX, 1);
+    amps_ReadDouble(file, &DY, 1);
+    amps_ReadDouble(file, &DZ, 1);
+
+    amps_ReadInt(file, &P, 1);
+  }
+
+
+  ForSubgridI(is, subgrids)
+  {
+    subgrid = SubgridArraySubgrid(subgrids, is);
+
+    int ix, iy, iz;
+    int nx, ny, nz;
+    int rx, ry, rz;
+    int i, j, k;
+
+    amps_ReadInt(file, &ix, 1);
+    amps_ReadInt(file, &iy, 1);
+    amps_ReadInt(file, &iz, 1);
+
+    amps_ReadInt(file, &nx, 1);
+    amps_ReadInt(file, &ny, 1);
+    amps_ReadInt(file, &nz, 1);
+
+    amps_ReadInt(file, &rx, 1);
+    amps_ReadInt(file, &ry, 1);
+    amps_ReadInt(file, &rz, 1);
+
+    GrGeomInLoop(i, j, k, gr_domain, rx, ix, iy, iz, nx, ny, nz,
+    {
+      int chem_index = (i-ix) + (j-iy) * nx + (k-iz) * nx * ny;
+
+      for (int idx = 0; idx < chem_sizes->num_primary; idx++)
+      {
+        amps_ReadDouble(file, &chem_state[chem_index].total_mobile.data[idx], 1);
+      }
+
+
+      for (int idx = 0; idx < chem_sizes->num_minerals; idx++)
+      {
+        amps_ReadDouble(file, &chem_state[chem_index].mineral_volume_fraction.data[idx], 1);
+        amps_ReadDouble(file, &chem_state[chem_index].mineral_specific_surface_area.data[idx], 1);
+        amps_ReadDouble(file, &chem_properties[chem_index].mineral_rate_cnst.data[idx], 1);
+      }
+
+
+      for (int idx = 0; idx < chem_sizes->num_surface_sites; idx++)
+      {
+        amps_ReadDouble(file, &chem_state[chem_index].surface_site_density.data[idx], 1);
+      }
+
+
+      for (int idx = 0; idx < chem_sizes->num_ion_exchange_sites; idx++)
+      {
+        amps_ReadDouble(file, &chem_state[chem_index].cation_exchange_capacity.data[idx], 1);
+      }
+
+
+      for (int idx = 0; idx < chem_sizes->num_isotherm_species; idx++)
+      {
+        amps_ReadDouble(file, &chem_properties[chem_index].isotherm_kd.data[idx], 1);
+        amps_ReadDouble(file, &chem_properties[chem_index].freundlich_n.data[idx], 1);
+        amps_ReadDouble(file, &chem_properties[chem_index].langmuir_b.data[idx], 1);
+      }
+
+
+      for (int idx = 0; idx < chem_sizes->num_aqueous_kinetics; idx++)
+      {
+        amps_ReadDouble(file, &chem_properties[chem_index].aqueous_kinetic_rate_cnst.data[idx], 1);
+      }
+
+
+      for (int idx = 0; idx < chem_sizes->num_aux_doubles; idx++)
+      {
+        amps_ReadDouble(file, &chem_aux_data[chem_index].aux_doubles.data[idx], 1);
+      }
+
+
+      if (chem_sizes->num_sorbed > 0)
+      {
+        for (int idx = 0; idx < chem_sizes->num_primary; idx++)
+          amps_ReadDouble(file, &chem_state[chem_index].total_immobile.data[idx], 1);
+      }
+
+
+      for (int idx = 0; idx < chem_sizes->num_aux_integers; idx++)
+      {
+        amps_ReadInt(file, &chem_aux_data[chem_index].aux_ints.data[idx], 1);
+      }
+
+    });
+
+  }
+  amps_FFclose(file);
+  EndTiming(PFBTimingIndex);
+}
 
